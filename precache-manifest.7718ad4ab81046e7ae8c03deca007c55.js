58024fa9ff8d63be4e3b8b9a50db41324570156d
Danis Preldzic <danispreldzicappleid@gmail.com>
2019-07-22 00:00:32 +0200

    Updates

---
+++ precache-manifest.7718ad4ab81046e7ae8c03deca007c55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bf6e217bbe733e5931c69fdd91803c",
+    "url": "/starwarsnews/index.html"
+  },
+  {
+    "revision": "244410bee9e9eb9aaf8e",
+    "url": "/starwarsnews/static/css/main.1aed7751.chunk.css"
+  },
+  {
+    "revision": "424abb81bd7544421042",
+    "url": "/starwarsnews/static/js/2.3cb9c6a8.chunk.js"
+  },
+  {
+    "revision": "244410bee9e9eb9aaf8e",
+    "url": "/starwarsnews/static/js/main.d7a9c500.chunk.js"
+  },
+  {
+    "revision": "72e92cdcbb6bcc032ea6",
+    "url": "/starwarsnews/static/js/runtime~main.7e65a84d.js"
+  }
+]);
